dad6cc2cc0e4d072879efa62fe6cdb2d8afff563
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-02-01 08:11:15 -0800

    fixed

--- js/iterating.js
+++ js/iterating.js
@@ -27,7 +27,7 @@
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    for(var i = 0; i < 4; i++) {
+    for(var i = 0; i < names.length; i++) {
         console.log(names[i]);
     }
     /**
